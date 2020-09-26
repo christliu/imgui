9a63a47b78556d5e59929e53cb0ccbb5f57d23ca
christliu <215688426@qq.com>
2020-09-26 16:39:18 +0800

    imgui premake build script done..

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,29 @@
 project "ImGui"
+	-- location "Christ/external/imgui"
+	kind "StaticLib"
+	language "c++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		-- "%{prj.name}/**.h",
+		-- "%{prj.name}/**.cpp"
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		staticruntime "On"
+		systemversion "latest"
+		defines
+		{
+		}
